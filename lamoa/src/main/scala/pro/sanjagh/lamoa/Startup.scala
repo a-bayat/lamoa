ebb42433ef41fe8d6ca8802bbe7adc0971062102
Amirhossein <amirhossein.bayyat@gmail.com>
2021-08-07 18:17:30 +0430

    Readme todo's (#2)
    * Todo Description (#1)
    
    * Add File Handler to maintain file operation such as read data and get video type only.
    
    * complete README Todo list
    
    * Revert "complete README Todo list"
    
    This reverts commit f28e91cf96045cf74f1b70dac7f69fa28b78b748.
    
    * complete README Todo list

--- lamoa/src/main/scala/pro/sanjagh/lamoa/Startup.scala
+++ lamoa/src/main/scala/pro/sanjagh/lamoa/Startup.scala
@@ -1,15 +1,23 @@
 package pro.sanjagh.lamoa
 
 import com.colofabrix.scala.figlet4s.options.HorizontalLayout
-import com.colofabrix.scala.figlet4s.unsafe.{FIGureOps, Figlet4s, OptionsBuilderOps}
+import com.colofabrix.scala.figlet4s.unsafe.{
+  FIGureOps,
+  Figlet4s,
+  OptionsBuilderOps
+}
+
+import domain.FileOperations._
 
 object Startup extends App {
-  private def Banner(): Unit =
+  private def banner(): Unit =
     Figlet4s
       .builder("LAMOA")
       .withHorizontalLayout(HorizontalLayout.FullWidth)
       .render()
       .print()
+  banner()
 
-  Banner()
+  val mediaFile = getMediaFiles(readDir)
+
 }
