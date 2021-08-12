7c6d3eb64fbf801778b570cfb60f6e6aff096452
Amirhossein <amirhossein.bayyat@gmail.com>
2021-08-12 11:55:14 +0430

    - Added String Executor to extract the pure name of movie - Added typesafe config Dependencies

--- lamoa/src/main/scala/pro/sanjagh/lamoa/Startup.scala
+++ lamoa/src/main/scala/pro/sanjagh/lamoa/Startup.scala
@@ -1,23 +1,18 @@
 package pro.sanjagh.lamoa
 
 import com.colofabrix.scala.figlet4s.options.HorizontalLayout
-import com.colofabrix.scala.figlet4s.unsafe.{
-  FIGureOps,
-  Figlet4s,
-  OptionsBuilderOps
-}
+import com.colofabrix.scala.figlet4s.unsafe.{FIGureOps, Figlet4s, OptionsBuilderOps}
+import pro.sanjagh.lamoa.domain.MovieFactory
 
-import domain.FileOperations._
 
 object Startup extends App {
   private def banner(): Unit =
     Figlet4s
-      .builder("LAMOA")
-      .withHorizontalLayout(HorizontalLayout.FullWidth)
-      .render()
-      .print()
+     .builder("LAMOA")
+     .withHorizontalLayout(HorizontalLayout.FullWidth)
+     .render()
+     .print()
   banner()
 
-  val mediaFile = getMediaFiles(readDir)
-
+  MovieFactory.Ignite()
 }
