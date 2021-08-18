d4a090c57e67809a24ffb633f1dcca7235d0c7e6
Amirhossein <amirhossein.bayyat@gmail.com>
2021-08-18 19:12:58 +0430

    added connection through proxy connection

--- src/main/scala/pro/sanjagh/lamoa/domain/Configuration.scala
+++ src/main/scala/pro/sanjagh/lamoa/domain/Configuration.scala
@@ -1,10 +1,36 @@
 package pro.sanjagh.lamoa.domain
 
 import com.typesafe.config.Config
+import pro.sanjagh.lamoa.model.Proxy
+
+import scala.util.Try
 
 object Configuration {
 
   import com.typesafe.config.ConfigFactory
   val get: Config = ConfigFactory.load("application.conf").getConfig("lamoa")
 
+  private[domain] def getSubtitleUrl: String = {
+    Option(
+      Configuration.get.getConfig("server.subtitle").getString("address")
+    ).get
+  }
+
+  private[domain] def getTimeout: Int = {
+    Try(Configuration.get.getConfig("server.subtitle").getInt("timeout"))
+      .getOrElse(5000)
+  }
+
+  def connection: Proxy = {
+    val proxyConf = Configuration.get.getConfig("config").getConfig("proxy")
+    Proxy(proxyConf)
+  }
+
+  def getIpV4Regex: String = {
+    Option(Configuration.get.getConfig("config").getString("ip.v4_pattern")).get
+  }
+
+  def getIpV6Regex: String = {
+    Option(Configuration.get.getConfig("config").getString("ip.v6_pattern")).get
+  }
 }
